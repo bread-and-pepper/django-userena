832c9015a7829ef42cb0f9ee86fefb4d0ae6e3b9
Petar Radosevic <petar@wunki.org>
2010-06-25 16:12:23 +0200

    Fleshing things out..

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(name='django-userenity',
+      version='0.1.0',
+      description='Complete user management application for Django',
+      author='Petar Radosevic',
+      author_email='petar@wunki.org',
+      url='http://github.com/wunki/django-userenity',
+      download_url='http://github.com/wunki/django-userenity/downloads',
+      packages=['accounts'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+      )
