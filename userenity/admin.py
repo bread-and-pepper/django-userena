d0d4f46bb6003db4697aaa946849bc67453f12ad
Petar Radosevic <petar@wunki.org>
2010-06-30 15:50:19 +0200

    Refactored account inlines

--- userenity/admin.py
+++ userenity/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 from userenity.models import Account
-from userenity.forms import AccountForm
 
-class AccountAdmin(admin.ModelAdmin):
-    form = AccountForm
+admin.site.unregister(User)
+class AccountInline(admin.StackedInline):
+    model = Account
 
-admin.site.register(Account, AccountAdmin)
+class AccountAdmin(UserAdmin):
+    inlines = [AccountInline]
+
+admin.site.register(User, AccountAdmin)
