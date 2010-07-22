e8be972faa0f7d5df1f0523a9d6f19d4fc5fde0d
Petar Radosevic <petar@wunki.org>
2010-07-22 13:22:49 +0200

    Hopefull fix for redirect loop

--- userena/decorators.py
+++ userena/decorators.py
@@ -3,9 +3,17 @@ from django.http import HttpResponsePermanentRedirect
 
 from userena import settings as userena_settings
 
+def is_secure_forwarded(request):
+    if 'HTTP_X_FORWARDED_SSL' in request.META:
+        return request.META['HTTP_X_FORWARDED_SSL'] == 'on'
+    return False
+
 def secure_required(view_func):
     """ Decorator makes sure URL is accessed over https. """
     def _wrapped_view_func(request, *args, **kwargs):
+        if 'HTTP_X_FORWARDED_SSL' in request.META:
+            request.is_secure = lambda: request.META['HTTP_X_FORWARDED_SSL'] == 'on'
+
         if not request.is_secure():
             if userena_settings.USERENA_USE_HTTPS:
                 request_url = request.build_absolute_uri(request.get_full_path())
