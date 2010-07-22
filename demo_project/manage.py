6a29c531e33063e8337abc4bfa206d934f813733
Petar Radosevic <petar@wunki.org>
2010-07-22 10:13:07 +0200

    Added production settings

--- demo_project/manage.py
+++ demo_project/manage.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 from django.core.management import execute_manager
 try:
-    import settings # Assumed to be in the same directory.
+    import socket
+    if socket.gethostname() == 'breadandpepper.com':
+        import settings_production as settings
+    else:
+        import settings
 except ImportError:
     import sys
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
