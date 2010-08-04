e60f1fe382d2408e3a778977a01e1fdbf4f03153
Petar Radosevic <petar@wunki.org>
2010-08-04 19:13:42 +0200

    Improved admin display by collapsing account management fields.

--- userena/admin.py
+++ userena/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext as _
 
 from userena.models import Account
 
@@ -8,6 +9,18 @@ from userena.models import Account
 admin.site.unregister(User)
 
 class AccountInline(admin.StackedInline):
+    fieldsets = (
+        (None, {
+            'fields': ('mugshot', 'gender', 'website', 'location',
+                       'birth_date', 'about_me')
+        }),
+        (_('Account management fields'), {
+            'classes': ('collapse',),
+            'fields': ('last_active', 'activation_key',
+                       'activation_key_created', 'activation_notification_send',
+                       'email_new', 'email_verification_key')
+        }),
+    )
     model = Account
 
 class AccountAdmin(UserAdmin):
