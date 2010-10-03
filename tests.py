c11cef2d8b463f571421f36275b7dcfef0092f95
Petar Radosevic <petar@wunki.org>
2010-10-04 01:40:35 +0200

    Again, small fixes

--- tests.py
+++ tests.py
@@ -8,6 +8,17 @@ import sys
 os.environ["DJANGO_SETTINGS_MODULE"] = 'demo_project.settings'
 from demo_project import settings
 
+settings.INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.sessions',
+    'django.contrib.contenttypes',
+    'django.contrib.admin',
+    'django.contrib.sites',
+    'demo_project.profiles',
+    'userena',
+    'userena.tests',
+)
+
 def main():
     from django.test.utils import get_runner
     test_runner = get_runner(settings)(interactive=False)
