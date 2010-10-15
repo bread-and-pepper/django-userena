227b83a3e803f44dea98dcf3d3b869cd6a15db12
Petar Radosevic <petar@wunki.org>
2010-10-15 16:48:54 +0200

    Date joined

--- userena/admin.py
+++ userena/admin.py
@@ -12,6 +12,8 @@ class UserenaSignupInline(admin.StackedInline):
 
 class UserenaAdmin(UserAdmin):
     inlines = [UserenaSignupInline, ]
+    list_display = ('username', 'email', 'first_name', 'last_name',
+                    'is_staff', 'date_joined')
 
 admin.site.unregister(User)
 admin.site.register(User, UserenaAdmin)
