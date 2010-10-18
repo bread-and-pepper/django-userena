89bd4d6ef3ec4d9b5a3f194f0a8ff306724b4fc5
Petar Radosevic <petar@wunki.org>
2010-10-18 11:07:22 +0200

    Forms shouldn't have ugettext_lazy

--- userena/forms.py
+++ userena/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 
