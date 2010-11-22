dd2311201cd22ad986915638b0598d4b869988a0
Petar Radosevic <petar@wunki.org>
2010-11-22 17:44:27 +0100

    Added 100% test coverage for models.

--- userena/contrib/messages/tests/__init__.py
+++ userena/contrib/messages/tests/__init__.py
@@ -1,1 +1,2 @@
+from userena.contrib.messages.tests.models import *
 from userena.contrib.messages.tests.views import *
---
+++ userena/contrib/messages/tests/models.py
@@ -0,0 +1,49 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+from userena.contrib.messages.models import Message, MessageRecipient
+
+class MessageModelTests(TestCase):
+    fixtures = ['users', 'messages']
+
+    def test_string_formatting(self):
+        """ Test the human representation of a message """
+        message = Message.objects.get(pk=1)
+        self.failUnlessEqual(message.__unicode__(),
+                             message.subject)
+
+    def test_absolute_url(self):
+        """ Test the absolute url of a message """
+        message = Message.objects.get(pk=1)
+        self.failUnlessEqual(message.get_absolute_url(),
+                             reverse('userena_messages_detail',
+                                     kwargs={'message_id': 1}))
+
+class MessageRecipientModelTest(TestCase):
+    fixtures = ['users', 'messages']
+
+    def test_string_formatting(self):
+        """ Test the human representation of a recipient """
+        recipient = MessageRecipient.objects.get(pk=1)
+
+        valid_unicode = '%s (%s)' % (recipient.message,
+                                     recipient.user)
+
+        self.failUnlessEqual(recipient.__unicode__(),
+                             valid_unicode)
+
+    def test_new(self):
+        """ Test if the message that is new is correct """
+        new_message = MessageRecipient.objects.get(pk=1)
+        read_message = MessageRecipient.objects.get(pk=2)
+
+        self.failUnless(new_message.is_read())
+        self.failIf(read_message.is_read())
+
+    def test_replied(self):
+        """ Test that replied messages are correctly shown """
+        not_replied_message = MessageRecipient.objects.get(pk=1)
+        replied_message = MessageRecipient.objects.get(pk=2)
+
+        self.failIf(not_replied_message.is_replied())
+        self.failUnless(replied_message.is_replied())
