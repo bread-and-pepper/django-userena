161449a00456f325191d1cc24f7fe4a86fa6684b
Petar Radosevic <petar@wunki.org>
2010-11-26 10:26:33 +0100

    Better wording of choosing password. People got confused

--- userena/forms.py
+++ userena/forms.py
@@ -28,10 +28,10 @@ class SignupForm(forms.Form):
                              label=_("Email"))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs=attrs_dict,
                                                            render_value=False),
-                                label=_("Password"))
+                                label=_("Choose password"))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs=attrs_dict,
                                                            render_value=False),
-                                label=_("Confirm password"))
+                                label=_("Repeat password"))
 
     def clean_username(self):
         """
