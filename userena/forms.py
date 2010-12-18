5281932597776abbe9ff9f8307e3857e00127aef
Petar Radosevic <petar@wunki.org>
2010-12-18 11:33:09 +0100

    Updated error message for identification field.

--- userena/forms.py
+++ userena/forms.py
@@ -111,36 +111,41 @@ class SignupFormTos(SignupForm):
                              label=_(u'I have read and agree to the Terms of Service'),
                              error_messages={'required': _('You must agree to the terms to register.')})
 
-def identification_field_factory(label):
+def identification_field_factory(label, error_required):
     """
     A simple identification field factory which enable you to set the label.
 
     :param label:
         String containing the label for this field.
 
+    :param error_required:
+        String containing the error message if the field is left empty.
+
     """
     return forms.CharField(label=_("%(label)s") % {'label': label},
                            widget=forms.TextInput(attrs=attrs_dict),
                            max_length=75,
-                           error_messages={'required': _('Either supply us with your email or username.')})
+                           error_messages={'required': _("%(error)s") % {'error': error_required}})
 
 class AuthenticationForm(forms.Form):
     """
     A custom form where the identification can be a e-mail address or username.
 
     """
-    identification = identification_field_factory("Email or username")
+    identification = identification_field_factory("Email or username",
+                                                  "Either supply us with your email or username.")
     password = forms.CharField(label=_("Password"),
                                widget=forms.PasswordInput(attrs=attrs_dict, render_value=False))
     remember_me = forms.BooleanField(widget=forms.CheckboxInput(attrs=attrs_dict),
                                      required=False,
-                                     label=_(u'Remember me for %(days)s' % {'days': userena_settings.USERENA_REMEMBER_ME_DAYS[0]}))
+                                     label=_(u'Remember me for %(days)s') % {'days': userena_settings.USERENA_REMEMBER_ME_DAYS[0]})
 
     def __init__(self, *args, **kwargs):
         """ A custom init because we need to change the label if no usernames is used """
         super(AuthenticationForm, self).__init__(*args, **kwargs)
         if userena_settings.USERENA_WITHOUT_USERNAMES:
-            self.fields['identification'] = identification_field_factory("Email")
+            self.fields['identification'] = identification_field_factory("Email",
+                                                                         "Please supply your email.")
 
     def clean(self):
         """
