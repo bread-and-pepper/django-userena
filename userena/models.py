0a0cf86ba39d7b7e52e9f745bfadb3e90f3cdf67
Tom Vaughan <tom@software6.net>
2012-01-25 21:56:25 -0300

    Do not use usernames in email templates when usernames are disabled in userena.

--- userena/models.py
+++ userena/models.py
@@ -118,6 +118,7 @@ class UserenaSignup(models.Model):
 
         """
         context= {'user': self.user,
+                  'without_usernames': userena_settings.USERENA_WITHOUT_USERNAMES,
                   'new_email': self.email_unconfirmed,
                   'protocol': get_protocol(),
                   'confirmation_key': self.email_confirmation_key,
@@ -179,6 +180,7 @@ class UserenaSignup(models.Model):
 
         """
         context= {'user': self.user,
+                  'without_usernames': userena_settings.USERENA_WITHOUT_USERNAMES,
                   'protocol': get_protocol(),
                   'activation_days': userena_settings.USERENA_ACTIVATION_DAYS,
                   'activation_key': self.activation_key,
