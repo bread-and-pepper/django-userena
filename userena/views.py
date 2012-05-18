7218cc82d818b62f9a407dd7295c042fb9cddc78
Boris Chervenkov <boris.chervenkov@sentido.bg>
2012-05-18 03:32:02 +0300

    Fixes HTTP 405 response when a user tries to sign up, but the form contains errors

--- userena/views.py
+++ userena/views.py
@@ -35,6 +35,9 @@ class ExtraContextTemplateView(TemplateView):
         if self.extra_context:
             context.update(self.extra_context)
         return context
+
+    # this view is used in POST requests, e.g. signup when the form is not valid
+    post = TemplateView.get
         
 class ProfileListView(ListView):
     """ Lists all profiles """
