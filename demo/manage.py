a9dc3e0015b97557193d6c0f3886b6535e746750
Petar Radosevic <petar@wunki.org>
2013-10-14 13:37:31 +0200

    Use userena from the project root. Easier debugging.

--- demo/manage.py
+++ demo/manage.py
@@ -3,6 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
+    cwd = os.path.dirname(__file__)
+    sys.path.append(os.path.join(os.path.abspath(os.path.dirname(cwd)), '../'))
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo.settings")
 
     from django.core.management import execute_from_command_line
