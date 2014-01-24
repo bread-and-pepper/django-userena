edc3ab7b8fc7a9075c9ac026c385397babbf8cf8
swistakm <swistakm@gmail.com>
2014-01-24 15:20:58 +0100

    utilize first compatibility quirk for password reset view to pass password_reset tests in django>=1.6.0

--- userena/urls.py
+++ userena/urls.py
@@ -3,6 +3,14 @@ from django.contrib.auth import views as auth_views
 
 from userena import views as userena_views
 from userena import settings as userena_settings
+from userena.compat import auth_views_compat_quirks
+
+def merged_dict(dict_a, dict_b):
+    """Merges two dicts and returns output. It's purpose is to ease use of
+    ``auth_views_compat_quirks``
+    """
+    dict_a.update(dict_b)
+    return dict_a
 
 urlpatterns = patterns('',
     # Signup, signin and signout
@@ -19,14 +27,14 @@ urlpatterns = patterns('',
     # Reset password
     url(r'^password/reset/$',
        auth_views.password_reset,
-       {'template_name': 'userena/password_reset_form.html',
-        'email_template_name': 'userena/emails/password_reset_message.txt',
-        'extra_context': {'without_usernames': userena_settings.USERENA_WITHOUT_USERNAMES}
-        },
+       merged_dict({'template_name': 'userena/password_reset_form.html',
+                    'email_template_name': 'userena/emails/password_reset_message.txt',
+                    'extra_context': {'without_usernames': userena_settings.USERENA_WITHOUT_USERNAMES}
+                   }, auth_views_compat_quirks['userena_password_reset']),
        name='userena_password_reset'),
     url(r'^password/reset/done/$',
        auth_views.password_reset_done,
-       {'template_name': 'userena/password_reset_done.html'},
+       {'template_name': 'userena/password_reset_done.html',},
        name='userena_password_reset_done'),
     url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
        auth_views.password_reset_confirm,
