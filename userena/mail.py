aaaed59d47895aa844f660f8b88d2230b0e8ca80
leonardoo <leonardoorozcop@gmail.com>
2015-06-12 06:58:23 -0500

    refractor activation and confirmation mail

--- userena/mail.py
+++ userena/mail.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 import re
 
-from django.utils.six.moves import StringIO
+from django.conf import settings
+from django.template.loader import render_to_string
 from django.utils.translation import ugettext as _
 from django.core.mail import EmailMultiAlternatives
+
+from userena import settings as userena_settings
 
 from html2text import html2text
 
@@ -39,3 +42,41 @@ def send_mail(subject, message_plain, message_html, email_from, email_to,
 
 def wrap_attachment():
     pass
+
+
+class UserenaConfirmationMail(object):
+
+    _subject_txt = 'userena/emails/{}_email_subject{}.txt'
+    _subject_html = 'userena/emails/{}_email_message{}.html'
+
+    def __init__(self, context):
+        self.context = context
+
+    def generate_mail(self, type_mail, version=""):
+        self.type_mail = type_mail
+        self.subject_txt = self._subject_txt.format(type_mail, version)
+        self.subject_html = self._subject_html.format(type_mail, version)
+        self.subject = self._subject()
+        self.message_html = self._message_html()
+        self.message = self._message()
+
+    def send_mail(self, email):
+        send_mail(self.subject, self.message,
+                  self.message_html, settings.DEFAULT_FROM_EMAIL,
+                  [email])
+
+    def _message_html(self):
+        if userena_settings.USERENA_HTML_EMAIL:
+            return render_to_string(self.subject_html, self.context)
+        return None
+
+    def _message(self):
+        if (not userena_settings.USERENA_HTML_EMAIL or not self.message_html or
+            userena_settings.USERENA_USE_PLAIN_TEMPLATE):
+            return render_to_string(self.subject_txt, self.context)
+        return None
+
+    def _subject(self):
+        subject = render_to_string(self.subject_txt, self.context)
+        subject = ''.join(subject.splitlines())
+        return subject
