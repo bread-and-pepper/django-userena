22ecd2d6d5937357c40fc529df39d677e5bf7ff2
leonardoo <leonardoorozcop@gmail.com>
2015-06-12 07:39:58 -0500

    include subject and compat py2.6

--- userena/mail.py
+++ userena/mail.py
@@ -46,34 +46,37 @@ def wrap_attachment():
 
 class UserenaConfirmationMail(object):
 
-    _subject_txt = 'userena/emails/{}_email_subject{}.txt'
-    _subject_html = 'userena/emails/{}_email_message{}.html'
+    _message_txt = 'userena/emails/{0}_email_message{1}.txt'
+    _message_html = 'userena/emails/{0}_email_message{1}.html'
+    _subject_txt = 'userena/emails/{0}_email_subject{1}.txt'
 
     def __init__(self, context):
         self.context = context
 
     def generate_mail(self, type_mail, version=""):
         self.type_mail = type_mail
+        self.message_txt = self._message_txt.format(type_mail, version)
+        self.message_html = self._message_html.format(type_mail, version)
         self.subject_txt = self._subject_txt.format(type_mail, version)
-        self.subject_html = self._subject_html.format(type_mail, version)
         self.subject = self._subject()
-        self.message_html = self._message_html()
-        self.message = self._message()
+        self.message_html = self._message_in_html()
+        self.message = self._message_in_txt()
 
     def send_mail(self, email):
         send_mail(self.subject, self.message,
                   self.message_html, settings.DEFAULT_FROM_EMAIL,
                   [email])
 
-    def _message_html(self):
+    def _message_in_html(self):
         if userena_settings.USERENA_HTML_EMAIL:
-            return render_to_string(self.subject_html, self.context)
+            return render_to_string(self.message_html, self.context)
         return None
 
-    def _message(self):
-        if (not userena_settings.USERENA_HTML_EMAIL or not self.message_html or
-            userena_settings.USERENA_USE_PLAIN_TEMPLATE):
-            return render_to_string(self.subject_txt, self.context)
+    def _message_in_txt(self):
+        if (not userena_settings.USERENA_HTML_EMAIL
+            or not self.message_html
+            or userena_settings.USERENA_USE_PLAIN_TEMPLATE):
+            return render_to_string(self.message_txt, self.context)
         return None
 
     def _subject(self):
