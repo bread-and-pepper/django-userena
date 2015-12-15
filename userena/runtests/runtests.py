2bbec9514f8af8f27eb93722395bde3989256e04
Marco Schmalz <marco@schess.ch>
2015-12-15 22:01:22 +0100

    Fix runtest command

--- userena/runtests/runtests.py
+++ userena/runtests/runtests.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 # fix sys path so we don't need to setup PYTHONPATH
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'userena.runtests.settings'
 
 import django
